18185a31530c9105f2a9989e4529c88948cd1df4
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-01-03 18:22:37 +0530

    8 of 80 tests failed.
    pass tests/userprog/args-none
    pass tests/userprog/args-single
    pass tests/userprog/args-multiple
    pass tests/userprog/args-many
    pass tests/userprog/args-dbl-space
    pass tests/userprog/sc-bad-sp
    pass tests/userprog/sc-bad-arg
    pass tests/userprog/sc-boundary
    pass tests/userprog/sc-boundary-2
    pass tests/userprog/sc-boundary-3
    pass tests/userprog/halt
    pass tests/userprog/exit
    pass tests/userprog/create-normal
    pass tests/userprog/create-empty
    pass tests/userprog/create-null
    pass tests/userprog/create-bad-ptr
    pass tests/userprog/create-long
    pass tests/userprog/create-exists
    pass tests/userprog/create-bound
    pass tests/userprog/open-normal
    pass tests/userprog/open-missing
    pass tests/userprog/open-boundary
    pass tests/userprog/open-empty
    pass tests/userprog/open-null
    pass tests/userprog/open-bad-ptr
    pass tests/userprog/open-twice
    pass tests/userprog/close-normal
    pass tests/userprog/close-twice
    pass tests/userprog/close-stdin
    pass tests/userprog/close-stdout
    pass tests/userprog/close-bad-fd
    pass tests/userprog/read-normal
    pass tests/userprog/read-bad-ptr
    pass tests/userprog/read-boundary
    pass tests/userprog/read-zero
    pass tests/userprog/read-stdout
    pass tests/userprog/read-bad-fd
    pass tests/userprog/write-normal
    pass tests/userprog/write-bad-ptr
    pass tests/userprog/write-boundary
    pass tests/userprog/write-zero
    pass tests/userprog/write-stdin
    pass tests/userprog/write-bad-fd
    pass tests/userprog/exec-once
    pass tests/userprog/exec-arg
    pass tests/userprog/exec-bound
    pass tests/userprog/exec-bound-2
    FAIL tests/userprog/exec-bound-3
    pass tests/userprog/exec-multiple
    pass tests/userprog/exec-missing
    pass tests/userprog/exec-bad-ptr
    pass tests/userprog/wait-simple
    pass tests/userprog/wait-twice
    pass tests/userprog/wait-killed
    pass tests/userprog/wait-bad-pid
    pass tests/userprog/multi-recurse
    pass tests/userprog/multi-child-fd
    pass tests/userprog/rox-simple
    pass tests/userprog/rox-child
    pass tests/userprog/rox-multichild
    FAIL tests/userprog/bad-read
    FAIL tests/userprog/bad-write
    FAIL tests/userprog/bad-read2
    FAIL tests/userprog/bad-write2
    FAIL tests/userprog/bad-jump
    FAIL tests/userprog/bad-jump2
    FAIL tests/userprog/no-vm/multi-oom
    pass tests/filesys/base/lg-create
    pass tests/filesys/base/lg-full
    pass tests/filesys/base/lg-random
    pass tests/filesys/base/lg-seq-block
    pass tests/filesys/base/lg-seq-random
    pass tests/filesys/base/sm-create
    pass tests/filesys/base/sm-full
    pass tests/filesys/base/sm-random
    pass tests/filesys/base/sm-seq-block
    pass tests/filesys/base/sm-seq-random
    pass tests/filesys/base/syn-read
    pass tests/filesys/base/syn-remove
    pass tests/filesys/base/syn-write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,12 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "devices/shutdown.h"
+#include "devices/block.h"
+
+const int MIN_FILENAME = 1;
+const int MAX_FILENAME = 14;
 
 typedef int pid_t;    /* A data type that represents a process id. */
 
@@ -17,8 +23,19 @@ static struct lock filesys_lock;    /* The lock used for file system operations.
 
 static void syscall_handler (struct intr_frame *);
 bool is_valid_ptr(const void *ptr);
+bool is_valid_filename(const void *file);
+static void halt(void);
+void exit(int status);
 static pid_t exec(const char *cmd_line);
+static int wait(pid_t pid);
+static bool create(const char *file, unsigned initial_size);
+static bool remove(const char *file);
+static int open(const char *file);
+static filesize(int fd);
+static int read(int fd, void *buffer, unsigned size);
 static int write(int fd, const void *buffer, unsigned size);
+static void seek(int fd, unsigned position);
+static unsigned tell(int fd);
 static void close(int fd);
 
 void
@@ -47,11 +64,44 @@ syscall_handler (struct intr_frame *f UNUSED)
   uint32_t syscall_num = *esp;
   switch (syscall_num)
   {
+    case SYS_HALT:
+      halt();
+  		break;
     case SYS_EXIT:
       exit(*argv0);
       break;
+    case SYS_EXEC:
+      f->eax = exec((char *)*argv0);
+  		break;
+  	case SYS_WAIT:
+      f->eax = wait(*argv0);
+  		break;
+  	case SYS_CREATE:
+      f->eax = create((char *)*argv0, *argv1);
+  		break;
+  	case SYS_REMOVE:
+      f->eax = remove((char *)*argv0);
+  		break;
+  	case SYS_OPEN:
+      f->eax = open((char *)*argv0);
+  		break;
+  	case SYS_FILESIZE:
+      f->eax = filesize(*argv0);
+  		break;
+  	case SYS_READ:
+      f->eax = read(*argv0, (void *)*argv1, *argv2);
+  		break;
   	case SYS_WRITE:
   		f->eax = write(*argv0, (void *)*argv1, *argv2);
+  		break;
+    case SYS_SEEK:
+      seek(*argv0, *argv1);
+  		break;
+  	case SYS_TELL:
+      f->eax = tell(*argv0);
+  		break;
+  	case SYS_CLOSE:
+      close(*argv0);
   		break;
     default:
   		break;
@@ -70,6 +120,17 @@ is_valid_ptr(const void *ptr)
     || pagedir_get_page(thread_current()->pagedir, ptr) == NULL)
     return false;
   return true;
+}
+
+/* Check whether *file is a valid filename. */
+bool
+is_valid_filename(const void *file)
+{
+  if (!is_valid_ptr(file))
+    exit(-1);
+
+  int len = strlen(file);
+  return len >= MIN_FILENAME && len <= MAX_FILENAME;
 }
 
 struct file_descriptor *
@@ -117,6 +178,13 @@ close_openfile(int fd)
   return;
 }
 
+/* Terminates Pintos. */
+static void
+halt(void)
+{
+  shutdown_power_off();
+}
+
 /* Terminate the current user program.
    Return the status to the kernel.
    If the status is 0, it indicates success.
@@ -147,6 +215,181 @@ exit(int status)
   thread_exit();
 }
 
+/* Run the executable whose name is given in cmd_line,
+   passing any given arguments.
+   Return the new process's program id(pid).
+   Must return pid -1, which otherwise should not be a valid pid,
+   if the program cannot load or run for any reason. */
+static pid_t
+exec(const char *cmd_line)
+{
+  if (!is_valid_ptr(cmd_line))    /* Check if the cmd_line is a valid pointer in the user address space. */
+    exit(-1);
+
+  lock_acquire(&filesys_lock);    /* Acquire the lock for the file system before executing the new process. */
+  tid_t tid = process_execute(cmd_line);    /* Execute the new process and get its thread id(tid). */
+  lock_release(&filesys_lock);    /* Release the lock for the file system after executing the new process. */
+  
+  return tid;
+}
+
+/* Wait for a child process pid.
+   If pid is still alive, wait until it terminates.
+   Return the child's exit status. */
+static int
+wait(pid_t pid)
+{
+  return process_wait(pid);
+}
+
+/* Assign unique fd to a file.
+   Return fd. */
+int
+assign_fd()
+{
+  struct list *list = &thread_current()->fds;
+  if (list_empty(list))
+    return 2;
+  else
+  {
+    struct file_descriptor *f = 
+        list_entry(list_back(list), struct file_descriptor, fdelem);
+    return f->fd + 1;
+  }
+}
+
+/* Compare fd values as list_elem.
+   Return true if fd(a) < fd(b), otherwise false. */
+bool
+cmp_fd(const struct list_elem *a, const struct list_elem *b, void *aux)
+{
+  struct file_descriptor *left = list_entry(a, struct file_descriptor, fdelem);
+  struct file_descriptor *right = list_entry(b, struct file_descriptor, fdelem);
+  return left->fd < right->fd;
+}
+
+/* Create a new file called *file that has intial_size size.
+   Return true if successful, false otherwise. */
+static bool
+create(const char *file, unsigned initial_size)
+{
+  if (!is_valid_filename(file))   /* Check if the file name is valid. */
+    return false;
+
+  /* Acquire the file system lock to prevent other threads from accessing the file system. */
+  lock_acquire(&filesys_lock);
+
+  /* Create a new inode in the root directory with the specified initial size and
+     Add it to the directory. */
+  block_sector_t inode_sector = 0;
+  struct dir *dir = dir_open_root ();
+  bool success = (dir != NULL
+                  && free_map_allocate (1, &inode_sector)
+                  && inode_create (inode_sector, initial_size)
+                  && dir_add (dir, file, inode_sector));
+  if (!success && inode_sector != 0)
+    free_map_release (inode_sector, 1);
+  dir_close (dir);
+
+  lock_release(&filesys_lock);    /* Release the file system lock. */
+
+  return success;
+}
+
+/* Delete the file called *file.
+   Return true if successful, false otherwise. */
+static bool
+remove(const char *file)
+{
+  if (!is_valid_filename(file))
+    return false;
+
+  bool status;
+  lock_acquire(&filesys_lock);
+  status = filesys_remove(file);
+  lock_release(&filesys_lock);
+
+  return status;
+}
+
+/* Open the file called *file, assign the opened file a fd
+   and the current process should keep track of it in fds list.
+   Return fd if the file can be opend, otherwise -1. */
+static int
+open(const char *file)
+{
+  int fd = -1;
+
+  if (!is_valid_filename(file))
+    return fd;
+
+  lock_acquire(&filesys_lock);
+  struct list *list = &thread_current()->fds;
+  struct file *file_struct = filesys_open(file);
+  if (file_struct != NULL)
+  {
+    struct file_descriptor *tmp = malloc(sizeof(struct file_descriptor));
+    tmp->fd = assign_fd();
+    tmp->file = file_struct;
+    fd = tmp->fd;
+    list_insert_ordered(list, &tmp->fdelem, (list_less_func *)cmp_fd, NULL);
+  }
+  lock_release(&filesys_lock);
+
+  return fd;
+}
+
+/* Get the size of fd file.
+   Return its size. */
+static int
+filesize(int fd)
+{
+  int size = -1;
+
+  lock_acquire(&filesys_lock);
+  struct file_descriptor *file_descriptor = get_openfile(fd);
+    if (file_descriptor != NULL)
+      size = file_length(file_descriptor->file);
+  lock_release(&filesys_lock);
+
+  return size;
+}
+
+/* Read size bytes from fd into buffer.
+   Return the number of bytes actully read. */
+static int
+read(int fd, void *buffer, unsigned size)
+{
+  int status = -1;
+
+  if (!is_valid_ptr(buffer) || !is_valid_ptr(buffer + size - 1))
+    exit(-1);
+
+  lock_acquire(&filesys_lock);
+  if (fd == STDIN_FILENO) /* Fead from the keyboard.*/
+  {
+    uint8_t *p = buffer;
+    uint8_t c;
+    unsigned counter = 0;
+    while (counter < size && (c = input_getc()) != 0)
+    {
+      *p = c;
+      p++;
+      counter++;
+    }
+    *p = 0;
+    status = size - counter;
+  } else if (fd != STDOUT_FILENO)
+  {
+    struct file_descriptor *file_descriptor = get_openfile(fd);
+    if (file_descriptor != NULL)
+      status = file_read(file_descriptor->file, buffer, size);
+  }
+  lock_release(&filesys_lock);
+
+  return status;
+}
+
 /* Write size bytes from buffer to fd.
    Return the number of bytes actually written. */
 static int
@@ -171,6 +414,36 @@ write(int fd, const void *buffer, unsigned size)
     if (file_descriptor != NULL)
       status = file_write(file_descriptor->file, buffer, size);
   }
+  lock_release(&filesys_lock);
+
+  return status;
+}
+
+/* Change the next byte to be read/written in open fd to position,
+   expressed in bytes from the beginning of the file. */
+static void
+seek(int fd, unsigned position)
+{
+  lock_acquire(&filesys_lock);
+  struct file_descriptor *file_descriptor = get_openfile(fd);
+    if (file_descriptor != NULL)
+      file_seek(file_descriptor->file, position);
+  lock_release(&filesys_lock);
+
+  return;
+}
+
+/* Get the position of the next byte te be read/writen in open fd,
+   expressed in bytes from the beginning of the file. */
+static unsigned
+tell(int fd)
+{
+  int status = -1;
+
+  lock_acquire(&filesys_lock);
+  struct file_descriptor *file_descriptor = get_openfile(fd);
+    if (file_descriptor != NULL)
+      status = file_tell(file_descriptor->file);
   lock_release(&filesys_lock);
 
   return status;
